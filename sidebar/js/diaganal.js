a1f69e09d5b9031f4f068b22506a09cfc0ad9479
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-04-07 23:16:03 +0500

    rof:main

--- sidebar/js/diaganal.js
+++ sidebar/js/diaganal.js
@@ -1,8 +1,9 @@
 window.onload = function() {
 
 var chart = new CanvasJS.Chart("chartContainer", {
-	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	theme: "dark2", // "light1", "light2", "dark1", "dark2"
 	exportEnabled: true,
+	backgroundColor:"",
 	animationEnabled: true,
 
 	data: [{
@@ -25,3 +26,4 @@ var chart = new CanvasJS.Chart("chartContainer", {
 chart.render();
 
 }
+
