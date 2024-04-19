cf92da93dd5efdbb9393093b216ff7902a2ee53f
nurbek2107 <abduaxatov002@gmail.com>
2024-04-19 12:30:42 +0500

    rof:version 2.1
    Signed-off-by: nurbek2107 <abduaxatov002@gmail.com>

--- sidebar/js/bars.js
+++ sidebar/js/bars.js
@@ -1,7 +1,7 @@
 var options = {
     series: [{
         name: 'Net Profit',
-        data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+        data: [70, 55, 57, 56, 61, 58, 63, 60, 66]
     }, {
         name: 'Revenue',
         data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
