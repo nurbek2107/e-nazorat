99d66cb58ec28592926e2e7c028c8d4de7777b0c
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-05-05 15:11:55 +0500

    bar
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- sidebar/js/bars.js
+++ sidebar/js/bars.js
@@ -51,12 +51,14 @@ var options = {
 var chart = new ApexCharts(document.querySelector("#chart"), options);
 chart.render();
 
+
 new Chart(document.getElementById("bar-chart-horizontal"), {
     type: 'horizontalBar',
     data: {
         labels: ["Africa", "Asia", "Europe", "Latin America"],
         datasets: [
             {
+                
                 label: "Population (millions)",
                 backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#c45850"],
                 data: [2478, 5267, 734, 784]
